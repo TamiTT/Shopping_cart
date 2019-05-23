8e88ee4abb60e7c676512dbcd2d16cdabebfd5f8
TamiTT <tamara.trabucco@zhdk.ch>
2019-05-23 11:44:21 +0200

    23.5. first script move - 2 Balken

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+
+	document.addEventListener("DOMContentLoaded", function() {
+	  console.log('DOM is ready');
+
+	  var columns = document.getElementsByClassName('column');
+	  console.log('columns: ', columns);
+
+	  // looping over all columns (inside of HTML collection)
+	  // Using ECMAScript 6, older browsers need other solutions
+	  for (var column of columns) {
+	    column.addEventListener('click', function(event) {
+	      // remove is-expanded class from all elements
+	      // to do so, we loop over each item again
+	      for (var col of columns) {
+	        col.classList.remove('is-expanded');
+	      }
+	      // the event.target referts to the clicked item
+	      console.log('event.target: ', event.target);
+	      event.target.classList.add('is-expanded');
+	    });
+	  }
+	});
